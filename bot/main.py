ece4c8fdfb4d247f8632fb74bb26fdee93116f08
Evan Goldsmith <evangoldsmith1@gmail.com>
2024-01-01 16:57:48 -0500

    Change bot file to use new column order

--- bot/main.py
+++ bot/main.py
@@ -7,28 +7,30 @@ from sklearn.linear_model import LogisticRegression
 MODEL_PATH = 'bot/finalized_model.sav'
 
 def main():
-    scraper = Scraper(date="12/16/2023")
+    scraper = Scraper()
 
     data = scraper.get_todays_matchups()
     df = pd.DataFrame(data)
 
-    X = df.drop(['home_team', 'away_team'], axis=1)
+    X = df.drop(['date', 'home_team', 'away_team'], axis=1)
 
     # Normalize the features
     scaler = StandardScaler()
     X_scaled = scaler.fit_transform(X)
-
+    
     # Load the model
     model = load(MODEL_PATH)
     
     predictions = model.predict_proba(X_scaled)
+    print(df.loc[0])
     
     for i in range(len(data)):
         print(f'Home Team: {data[i]["home_team"]}, Away Team: {data[i]["away_team"]}')
 
         winner, prob = data[i]['home_team'] if predictions[i][1] > predictions[i][0] else data[i]['away_team'], max(predictions[i])
+        prob_percentage = prob * 100
         print(f'Projected Winner: {winner}')
-        print(f'Confidence: {prob}\n')
+        print(f'Probability: {prob_percentage:.1f}%')
 
 
 if __name__ == "__main__":
